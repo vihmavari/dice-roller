37869af1ceae8d75d5229572b43b2d915309a64f
vihmavari <vihmavari.03@gmail.com>
2026-01-02 22:39:04 +0300

    fix cfg

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/dice-roller/'
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/dice-roller/' : '/',
+  }
 })
